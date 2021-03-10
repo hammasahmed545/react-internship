7673b7d95bbfe5467f07d363f2e3a17a74b896ad
Hammas Ahmed <hammasahmed545@gmail.com>
2021-03-10 20:57:31 +0500

    Second Commit

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -8,6 +8,7 @@ const Topbar = () => {
                     <div className="row">
                         <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                             <div className="follow_us_on">
+
                                 <ul>
                                     <li>
                                         <a title="Facebook" >
@@ -47,13 +48,17 @@ const Topbar = () => {
                                         </a>
                                     </li>
                                 </ul>
+
                             </div>
                         </div>
                         <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 mail-call">
+
                             <div className="mail-to">
                                 <i className="flat flaticon-mail-black-envelope-symbol"> </i>info@bizler.com
                             </div>
+
                             <span className="separator-pipesign white"> </span>
+
                             <div className="call-on">
                                 <i className="flat flaticon-phone-receiver"> </i>0123-456-789
                             </div>
